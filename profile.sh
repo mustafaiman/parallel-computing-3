e57c734a6ca4112cd25c9f8079b76fd84893faab
mustafaiman <mustafa.iman@hazelcast.com>
2015-12-01 12:19:25 +0200

    profiling for omp

--- profile.sh
+++ profile.sh
@@ -1,2 +1,2 @@
-gcc -std=c99 -o lbp_sec_profiling lbp_sec.c util.c -pg
-for ((i=1;i<=17;i+=1)); do ./lbp_sec_profiling $i > reports-seq/output.k$i.txt; gprof lbp_sec_profiling gmon.out > reports-seq/report.k$i.txt; echo "profiling for k=$i of 17 done";  done;
+gcc -std=c99 -o lbp_omp_profiling lbp_omp.c util.c -pg
+for ((i=1;i<=17;i+=1)); do ./lbp_omp_profiling $i > reports-omp/output.k$i.txt; gprof lbp_omp_profiling gmon.out > reports-omp/report.k$i.txt; echo "profiling for k=$i of 17 done";  done;
